16d2f2cad1a480c76daba2f5a0106a35bfcd41bf
niklas <nikha310@student.liu.se>
2013-10-11 11:17:30 +0200

    Working foreground point cloud.

--- catkin_ws/src/background_modelling/src/background_modelling_node.cpp
+++ catkin_ws/src/background_modelling/src/background_modelling_node.cpp
@@ -3,6 +3,7 @@
 #include <pcl-1.6/pcl/point_cloud.h>
 #include <pcl-1.6/pcl/ros/conversions.h>
 #include <pcl-1.6/pcl/point_types.h>
+#include <pcl-1.6/pcl/visualization/cloud_viewer.h>
 #include <iostream>
 #include <sensor_msgs/image_encodings.h>
 #include <cv_bridge/cv_bridge.h>
@@ -28,7 +29,21 @@ cv::BackgroundSubtractorMOG2 * bg;
 std::vector<std::vector<cv::Point> > contours;
 std::vector<cv::Mat> channels;
 
+
+// Elements from projection matrix needed for PC construction
+float fx, fy, cx, cy;
+
+pcl::visualization::CloudViewer viewer("Clod viewer");
+
+std::vector<pcl::PointXYZ> cloud;
+
 namespace enc = sensor_msgs::image_encodings;
+
+float getWorldCoord(float f, float c, float zWorld, int screenCoord)
+{
+	return (screenCoord - c)*zWorld/f;
+}
+
 
 void imageCb(const sensor_msgs::ImageConstPtr& msg)
 {
@@ -41,7 +56,6 @@ void imageCb(const sensor_msgs::ImageConstPtr& msg)
     		ROS_ERROR("cv_bridge exception: %s", e.what());
     		return;
   	}
-	//cv::Mat frame(cv_ptr->image.size().width, cv_ptr->image.size().height, CV_8UC3,cv_ptr->image);
 	int nanCount = 0;
   	cv::Mat back, fore, frame;
 	for(int i = 0; i<=cv_ptr->image.rows;++i){
@@ -52,58 +66,56 @@ void imageCb(const sensor_msgs::ImageConstPtr& msg)
 			}
 		}
 	}
-		cout <<"nanCOunt: " << nanCount << "number of pixels "<< 640*480   << endl;
+		//cout <<"nanCOunt: " << nanCount << "number of pixels "<< 640*480   << endl;
 		channels.clear();
+		cv::Mat depth = cv_ptr->image;
 		channels.push_back(cv_ptr->image);
 		channels.push_back(cv_ptr->image);
 		channels.push_back(cv_ptr->image);
-		cout << "i am receiving something" << endl;
+
 		cv::merge(channels,frame);
 		bg->operator()(frame, fore, -1);
 		bg->getBackgroundImage(back);
-	//cv::erode(fore,fore,cv::Mat());
-        //cv::dilate(fore,fore,cv::Mat());
-        cv::findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
-        cv::drawContours(frame,contours,-1,cv::Scalar(0,0,255),2);
-       
+		cv::erode(fore,fore,cv::Mat());
+        cv::dilate(fore,fore,cv::Mat());
+        //cv::findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
+        //cv::drawContours(frame,contours,-1,cv::Scalar(0,0,255),2);
         cv::imshow("Foreground", fore);
-		cv::imshow("Frame",frame/6);
+		cv::imshow("Frame",frame/6.0f);
         cv::imshow("Background",back*50);
         cv::waitKey(10);
 
-       // for()
+        cloud.clear();
 
-        //pub_.publish(cv_ptr->toImageMsg());*/
+        float x,y,z;
+
+       for(int u = 0; u < fore.cols; u++)
+       {
+    	   for(int v = 0; v < fore.rows; v++)
+    	   {
+
+    		   if(fore.at<char>(v,u) != 0)
+    		   {
+    			   z = depth.at<float>(v,u);
+    			   x = getWorldCoord(fx,cx,z,u);
+    			   y = getWorldCoord(fy,cy,z,v);
+    			   cloud.push_back(pcl::PointXYZ(x,y,z));
+    		   }
+    	   }
+
+       }
 
 
 
+       // Visualization of PC
+       /*
+       pcl::PointCloud<pcl::PointXYZ>::Ptr myCloud(new pcl::PointCloud<pcl::PointXYZ>());
+       myCloud->insert(myCloud->begin(), cloud.begin(), cloud.end());
+       viewer.showCloud(myCloud);
+       myCloud->clear();
+       */
 }
-float getWorldCoord(float f, float c, float zWorld, int screenCoord)
-{
-	return (screenCoord - c)*zWorld/f;
-}
 
-/*void imageCb2(const sensor_msgs::ImageConstPtr& msg)
-{
-  cv_bridge::CvImagePtr cv_ptcamera_matrix:r;
-  cv::Mat outputPic;
-  try
-  {
-    	cv_ptr = cv_bridge::toCvCopy(msg, enc::TYPE_32FC1);
-	cv::Scalar meanPicture = cv::max(cv_ptr->img);
-	cv::
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("cv_bridge exception: %s", e.what());
-    return;
-  }
-  cout << "i am receiving something 2" << endl;
-  cv::namedWindow("benjamin2!", CV_WINDOW_AUTOSIZE);
-  cv::imshow("benjamin2!", cv_ptr->image);
-  cv::waitKey(10);
-  //image_pub_.publish(cv_ptr->toImageMsg());
-}*/
 int main (int argc, char** argv)
 {
 	// Import camera intrinsic
@@ -112,8 +124,7 @@ int main (int argc, char** argv)
 	sensor_msgs::CameraInfo camInfo;
 	camera_calibration_parsers::readCalibration(path, cameraName, camInfo);
 
-	// Elements from projection matrix needed for PC construction
-	float fx, fy, cx, cy;
+
 	fx = camInfo.P.elems[0];
 	cx = camInfo.P.elems[2];
 	fy = camInfo.P.elems[5];
@@ -125,7 +136,7 @@ int main (int argc, char** argv)
 	cv::namedWindow("Foreground");
 	ros::init (argc, argv, "testKinect");
   	ros::NodeHandle nh;
-	bg = new cv::BackgroundSubtractorMOG2(10000, 0.040f, false);
+	bg = new cv::BackgroundSubtractorMOG2(1000, 0.040f, false);
 	bg->set("nmixtures", 10);
 	cout << "olle är kingen!" << endl;
 
@@ -136,7 +147,6 @@ int main (int argc, char** argv)
   	// Create a ROS subscriber for the input point cloud
 
   	ros::Subscriber sub = nh.subscribe("/camera/depth/image", 10, imageCb);
-	//ros::Subscriber sub2= nh.subscribe ("/camera/depth/image", 10, imageCb2);
   	// Create a ROS publisher for the output point cloud
   	//pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
 
