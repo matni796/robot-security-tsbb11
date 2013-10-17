f71cc937608d383cf92a3cafaa0da438292bfcf5
niklas <nikha310@student.liu.se>
2013-10-17 11:05:39 +0200

    - Added normalization of images in Background and cleanup - CamMatRGB.yaml added

--- catkin_ws/src/background_modelling/src/background_modelling_node.cpp
+++ catkin_ws/src/background_modelling/src/background_modelling_node.cpp
@@ -24,17 +24,16 @@ using namespace std;
 ros::Publisher pub;
 
 //openCV stuff
-
 cv_bridge::CvImagePtr cv_ptr;
 cv::BackgroundSubtractorMOG2 * bg;	
 std::vector<std::vector<cv::Point> > contours;
 std::vector<cv::Mat> channels;
 
-
 // Elements from projection matrix needed for PC construction
 float fx, fy, cx, cy;
-//pcl::visualization::CloudViewer viewer("Clod viewer");
-std::vector<pcl::PointXYZ> cloud;
+const float normFact = 255.0f/6.0f;			// Normalization factor
+//pcl::visualization::CloudViewer viewer("Cloud viewer");
+std::vector<pcl::PointXYZ> cloud;				// Point cloud
 namespace enc = sensor_msgs::image_encodings;
 
 float getWorldCoord(float f, float c, float zWorld, int screenCoord)
@@ -42,101 +41,68 @@ float getWorldCoord(float f, float c, float zWorld, int screenCoord)
 	return (screenCoord - c)*zWorld/f;
 }
 
-
 void imageCb(const sensor_msgs::ImageConstPtr& msg)
 {
 	try
 	{
 		cv_ptr = cv_bridge::toCvCopy(msg, "");
-  	}
-  	catch (cv_bridge::Exception& e)
-  	{
-    		ROS_ERROR("cv_bridge exception: %s", e.what());
-    		return;
-  	}
+	}
+	catch (cv_bridge::Exception& e)
+	{
+		ROS_ERROR("cv_bridge exception: %s", e.what());
+		return;
+	}
 	int nanCount = 0;
-  	cv::Mat back, fore, frame;/*
-	for(int i = 1; i<=cv_ptr->image.rows;++i){
-		for(int k = 1; k<=cv_ptr->image.cols;++k){
-			if(cv_ptr->image.at<float>(i,k) != cv_ptr->image.at<float>(i,k)){
-				if(cv_ptr->image.at<float>(i-1,k) == cv_ptr->image.at<float>(i-1,k))
-					cv_ptr->image.at<float>(i,k) = cv_ptr->image.at<float>(i-1,k);
-				else if(cv_ptr->image.at<float>(i,k-1) == cv_ptr->image.at<float>(i,k-1))
-					cv_ptr->image.at<float>(i,k) = cv_ptr->image.at<float>(i,k-1);
-				else
-					cv_ptr->image.at<float>(i,k) = 5;
-				//cv_ptr->image.at<float>(i,k) = 0;
-				//++nanCount;
-			}
-		}
-	}*/
-	//cout <<"nanCOunt: " << nanCount << "number of pixels "<< 640*480   << endl;
+	cv::Mat back, fore, frame, depth, normImg;
+
+	cv_ptr->image.copyTo(depth);	// Copy depth image to create point cloud and vizualization
+
 	channels.clear();
-	cv::Mat depth = cv_ptr->image;
-	channels.push_back(cv_ptr->image);
-	channels.push_back(cv_ptr->image);
-	channels.push_back(cv_ptr->image);
+	cv_ptr->image = cv_ptr->image*normFact;			// Normalize image
+	cv_ptr->image.convertTo(normImg, CV_8UC1);		// Convert to uint8
+
+	channels.push_back(normImg);
+	channels.push_back(normImg);
+	channels.push_back(normImg);
 
 	cv::merge(channels,frame);
 	bg->operator()(frame, fore, -1);
 	bg->getBackgroundImage(back);
 
+	cv::erode(fore,fore,cv::Mat());
+	cv::dilate(fore,fore,cv::Mat());
+
+	cv::imshow("Foreground", fore);
+	cv::imshow("Frame",frame);
+	cv::imshow("Background",back);
+	cv::waitKey(10);
+	float x,y,z;
+	cloud.clear();
 
 	for(int u = 0; u < fore.cols; u++)
-	       {
-	    	   for(int v = 0; v < fore.rows; v++)
-	    	   {
-	    		   if (depth.at<float>(v,u) != depth.at<float>(v,u))
-	    			   fore.at<char>(v,u) = 0;
-	    	   }
-	       }
+	{
+		for(int v = 0; v < fore.rows; v++)
+		{
+			if(fore.at<char>(v,u) != 0)
+			{
+				z = depth.at<float>(v,u);
+				if(z != z)
+				{
+					x = getWorldCoord(fx,cx,z,u);
+					y = getWorldCoord(fy,cy,z,v);
+					cloud.push_back(pcl::PointXYZ(x,y,z));
+				}
+			}
+		}
+	}
 
-	cv::erode(fore,fore,cv::Mat());
-	cv::erode(fore,fore,cv::Mat());
-
-        cv::dilate(fore,fore,cv::Mat());
-        cv::dilate(fore,fore,cv::Mat());
-
-        //cv::findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
-        //cv::drawContours(frame,contours,-1,cv::Scalar(0,0,255),2);
-        cv::imshow("Foreground", fore);
-	cv::imshow("Frame",frame/6.0f);
-        cv::imshow("Background",back*50);
-        cv::waitKey(10);
-        cloud.clear();
-        float x,y,z;
-
-       for(int u = 0; u < fore.cols; u++)
-       {
-    	   for(int v = 0; v < fore.rows; v++)
-    	   {
-
-    		   if(fore.at<char>(v,u) != 0)
-    		   {
-    			   z = depth.at<float>(v,u);
-    			   if(z == z){
-    				   x = getWorldCoord(fx,cx,z,u);
-    				   y = getWorldCoord(fy,cy,z,v);
-    				   cloud.push_back(pcl::PointXYZ(x,y,z));
-    			   }
-    		   }
-    	   }
-
-       }
-	
-	
-
-       // Visualization of PC
-       
-       	pcl::PointCloud<pcl::PointXYZ>::Ptr myCloud(new pcl::PointCloud<pcl::PointXYZ>());
-      	myCloud->insert(myCloud->begin(), cloud.begin(), cloud.end());
-       	//sensor_msgs::PointCloud2 msg;
-	
-	//pcl::toROSMsg(myCloud, msg);
+	// Publish point cloud
+	pcl::PointCloud<pcl::PointXYZ>::Ptr myCloud(new pcl::PointCloud<pcl::PointXYZ>());
+	myCloud->insert(myCloud->begin(), cloud.begin(), cloud.end());
 	pub.publish(myCloud);
 	//viewer.showCloud(myCloud);
-       	myCloud->clear();
-       
+	myCloud->clear();
+
 }
 
 int main (int argc, char** argv)
@@ -153,25 +119,20 @@ int main (int argc, char** argv)
 	fy = camInfo.P.elems[5];
 	cy = camInfo.P.elems[6];
 
-  // Initialize ROS and openCV windows
- 	cv::namedWindow("Frame");
+	// Initialize ROS and openCV windows
+	cv::namedWindow("Frame");
 	cv::namedWindow("Background");
 	cv::namedWindow("Foreground");
 	ros::init (argc, argv, "testKinect");
-  	ros::NodeHandle nh;
-	bg = new cv::BackgroundSubtractorMOG2(1000, 0.040f, false);
+	ros::NodeHandle nh;
+	bg = new cv::BackgroundSubtractorMOG2(20000, 16.0f, false);
 	bg->set("nmixtures", 10);
 
-	//bg->set("nframes", 1000);
-	//bg.set("bShadowDetection", false);//bg.nmixtures = 3;
-	//bg.bShadowDetection = false;
-	//bg.nchannels = 1;
-  	// Create a ROS subscriber for the input point cloud
+	// Create a ROS subscriber for the input point cloud
+	ros::Subscriber sub = nh.subscribe("/camera/depth/image", 1, imageCb);
+	// Create a ROS publisher for the output point cloud
 	pub = nh.advertise<sensor_msgs::PointCloud2>("foreground_cloud",1);
-  	ros::Subscriber sub = nh.subscribe("/camera/depth/image", 1, imageCb);
-  	// Create a ROS publisher for the output point cloud
-  	//pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
 
-  	// Spin
-  	ros::spin ();
+	// Spin
+	ros::spin ();
 }
