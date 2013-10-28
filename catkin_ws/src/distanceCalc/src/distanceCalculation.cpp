3c180bf108b41795460371b8a7c0cf4795aba6c9
’PatrikHillgren’ <pathi747@gmail.com>
2013-10-28 08:48:43 +0100

    Tenta-p start.

--- catkin_ws/src/distanceCalc/src/distanceCalculation.cpp
+++ catkin_ws/src/distanceCalc/src/distanceCalculation.cpp
@@ -9,7 +9,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <stereo_msgs/DisparityImage.h>
-#include "std_msgs/String.h"
+#include <std_msgs/String.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/PointCloud.h>
 #include <pcl-1.6/pcl/ros/conversions.h>
@@ -26,41 +26,50 @@ ros::Subscriber sub;
 pcl::PointXYZ p;
 pcl::PointXYZ p2;
 pcl::PointXYZ minPoint;
-float distance;
-float minDistance= 10000;
 pcl::PointXYZ closestJoint;
 vector<pcl::PointXYZ> robotJoint(new pcl::PointXYZ(10,10,10),new pcl::PointXYZ(10,10,100));
 
-vector<pcl::PointXYZ> compareToRobot(pcl::PointXYZ ps){
-	float minDistance = 10000.0f;
+struct ReturnObject{
+	pcl::PointXYZ rj;
+	pcl::PointXYZ cp;
+	float dist;
+};
 
+vector<ReturnObject> returnVector;
+
+ReturnObject compareToRobot(pcl::PointXYZ ps){
+	float minDistance = std::numeric_limits<float>::infinity();
+	float distance;
 	for(int i = 0; i < robotJoint.size(); i++){
-		distance = powf(robotJoint[i].x-ps.x, 2)+powf(robotJoint[i].y-ps.y, 2)+powf(robotJoint[i].z-ps.z, 2);
+		distance = powf(robotJoint[i].x-ps.x, 2)+pow(robotJoint[i].y-ps.y, 2)+pow(robotJoint[i].z-ps.z, 2);
 		if (distance <= minDistance)
 		{
 			minDistance = distance;
 			minPoint = ps;
 			closestJoint = robotJoint[i];
 		}
-
 	}
+	ReturnObject clusterDistance;
+	clusterDistance.cp = ps;
+	clusterDistance.rj=closestJoint;
+	clusterDistance.dist= minDistance;
+	return clusterDistance;
 }
 void distanceCalc(vector<vector<pcl::PointXYZ> > vec){
-
 	for(int j = 0; j<vec.size();j++){
 		for(int i = 0; i<vec[j].size();i++){
-			p.x = vec[j][i].x;
-			p.y = vec[j][i].y;
-			p.z = vec[j][i].z;
-			distance = compareToRobot(p);
+			pcl::PointXYZ point = vec[j][i];
+			ReturnObject ro = compareToRobot(point);
+			returnVector.push_back(ro);
 		}
-
 	}
+
+	//chatter_pub.publish(returnVector);
 }
 int main (int argc, char** argv)
 {
 	ros::init (argc, argv, "distanceCalc");
-	sub = nh.subscribe ("cluster_cloud", 1,"distanceCalc");
-	chatter_pub = nh.advertise<sensor_msgs::PointCloud2>("distances", 1);
+	sub = nh.subscribe("cluster_cloud", 1,distanceCalc);
+	//chatter_pub = nh.advertise<ReturnObject>("distances", 1);
 	ros::spin ();
 }
