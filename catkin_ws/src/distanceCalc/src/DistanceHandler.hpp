ac58750733f205a8ef96352b174f2ee62ee1e7ad
Mattias Nilsson <matni796@student.liu.se>
2013-11-05 17:15:38 +0100

    FOrgot the file

---
+++ catkin_ws/src/distanceCalc/src/DistanceHandler.hpp
@@ -0,0 +1,35 @@
+//Class for handeling distances between objects and robot
+#ifndef DISTANCE_HANDLER_HPP
+#define DISTSNCE_HANDLER_HPP
+
+#include <ros/ros.h>
+// PCL specific includes
+#include <iostream>
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <image_transport/image_transport.h>
+#include <vector>
+#include <std_msgs/Float64MultiArray.h>
+
+template <>
+class DistanceHandler{
+private:
+	cv::Mat rotationMatrix;
+	ros::Subscriber calibrationSubscriber, robotSubscriber, clusteringSubsriber;
+
+
+
+public:
+	DistanceHandler(ros::NodeHandle nh){
+		calibrationSubscriber = nh.subscribe("calibration_data", 1, calibrationCallback);
+		//TODO add robotSubsriber
+		clusteringSubsriber = nh.subscribe("distance_calc", 1, distanceCallback);
+	}
+	void calibrationCallback(const std_msgs::Float64MultiArray& msg);
+	void distanceCallback(const sensor_msgs::ImageConstPtr& msg);
+};
+
+#endif
