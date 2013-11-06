af427a31223108618731eb4ab5dfaf9c66a0f108
Mattias Nilsson <matni796@student.liu.se>
2013-11-06 09:11:52 +0100

    working class DistanceHandler

--- catkin_ws/src/distanceCalc/src/DistanceHandler.hpp
+++ catkin_ws/src/distanceCalc/src/DistanceHandler.hpp
@@ -14,7 +14,7 @@
 #include <vector>
 #include <std_msgs/Float64MultiArray.h>
 
-template <>
+//template <>
 class DistanceHandler{
 private:
 	cv::Mat rotationMatrix;
@@ -24,9 +24,9 @@ private:
 
 public:
 	DistanceHandler(ros::NodeHandle nh){
-		calibrationSubscriber = nh.subscribe("calibration_data", 1, calibrationCallback);
+		calibrationSubscriber = nh.subscribe("calibration_data", 1, &DistanceHandler::calibrationCallback, this);
 		//TODO add robotSubsriber
-		clusteringSubsriber = nh.subscribe("distance_calc", 1, distanceCallback);
+		clusteringSubsriber = nh.subscribe("distance_calc", 1,  &DistanceHandler::distanceCallback, this);
 	}
 	void calibrationCallback(const std_msgs::Float64MultiArray& msg);
 	void distanceCallback(const sensor_msgs::ImageConstPtr& msg);
